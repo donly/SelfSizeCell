8ed98eab738c1eb2056c171ee2de8dbe8acf551c
donly <donly@magicalboy.com>
2015-07-31 11:50:46 +0800

    Initial Commit

---
+++ SelfSizeCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfSizeCell
+//
+//  Created by Donly Chan on 15/7/31.
+//  Copyright (c) 2015年 Donly Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfSizeCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfSizeCell
+//
+//  Created by Donly Chan on 15/7/31.
+//  Copyright (c) 2015年 Donly Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
